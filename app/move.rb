b4816b50ae788f87dd8f875900f028439f681374
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2021-09-15 00:20:21 +0000

    Do not reflect board

--- app/move.rb
+++ app/move.rb
@@ -13,8 +13,8 @@ end
 def can_move_direction?(board, me, direction)
   return false if direction == "up" && me[:head][:y] >= board[:height] - 1
   return false if direction == "down" && me[:head][:y] == 0 
-  return false if direction == "left" && me[:head][:x] >= board[:width] - 1
-  return false if direction == "right" && me[:head][:x] == 0
+  return false if direction == "left" && me[:head][:x] == 0
+  return false if direction == "right" && me[:head][:x] == board[:width] - 1
   return true
 end
 
--- spec/move_spec.rb
+++ spec/move_spec.rb
@@ -28,4 +28,19 @@ describe "move" do
       is_expected.to eq({move: "left", shout: "I'm at 5, 10!"})
     end
   end
+
+  context "when Hettie is about to run into a wall" do
+    let(:request) do
+      create_move_request({ you: {
+        head: {
+          x: 0,
+          y: 10 }
+        }
+      })
+    end
+
+    it "moves left" do
+      is_expected.to eq({move: "down", shout: "I'm at 0, 10!"})
+    end
+  end
 end
