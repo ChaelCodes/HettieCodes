659725f2fe3088f515481c2fe639912ebc447e55
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2021-09-15 00:09:06 +0000

    Hettie does not run into walls. They move in a box whenever they get close to one!

--- app/move.rb
+++ app/move.rb
@@ -1,10 +1,26 @@
 # This function is called on every turn of a game. It's how your Battlesnake decides where to move.
 # Valid moves are "up", "down", "left", or "right".
-# TODO: Use the information in board to decide your next move.
-def move(board)
-  puts board
+POSSIBLE_MOVES = ["up", "left", "down", "right"]
 
-  # Choose a random direction to move in
-  possible_moves = ["up", "left", "down", "right"]
-  { "move": possible_moves.first }
+def move(request)
+  board = request[:board]
+  me = request[:you]
+  POSSIBLE_MOVES.each do |direction|
+    return move_direction(direction) if can_move_direction?(board, me, direction)
+  end
+end
+
+def can_move_direction?(board, me, direction)
+  return false if direction == "up" && me[:head][:y] == 0
+  return false if direction == "down" && me[:head][:y] == board[:height] - 1
+  return false if direction == "left" && me[:head][:x] == 0
+  return false if direction == "right" && me[:head][:x] == board[:width] - 1
+  return true
+end
+
+def move_direction(direction)
+  {
+    "move": direction,
+    "shout": "Let's move #{direction}!"
+  }
 end
